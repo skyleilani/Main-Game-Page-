d5bebad2e6fb5dbc2afff5c0982102343dcdaade
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-01-21 21:59:44 -0800

    Update MainPage.js

--- MainPage.js
+++ MainPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import ListUsernames from './components/ListUsernames';
 import NewUser from './components/NewUser';
 import ToggleGamesPlayed from './components/ToggleGamesPlayed';
