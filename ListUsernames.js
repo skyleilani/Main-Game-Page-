cd1c765f4134198c393456ad9312b48ed9f0f8cf
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-01-21 22:32:40 -0800

    Update and rename ListUsernames.jsx to ListUsernames.js

---
+++ ListUsernames.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Username from './Username';
+import PropTypes from 'prop-types';
+
+// Display all created users in divs
+const ListUsernames = ({isToggleOn, gamesPlayed, usernames}) => {
+  return (
+    <div>
+      <ul className='user'>
+        {usernames.map((u, index) => (
+          <Username
+            key={index}
+            username={u}
+            isToggleOn={isToggleOn}
+            gamesPlayed={gamesPlayed}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+ListUsernames.propTypes = {
+  usernames: PropTypes.array.isRequired,
+};
+
+export default ListUsernames;
